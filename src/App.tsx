5c480009a8d8bd49e06ece86daac47189ec8643e
ashirbad <ashirbadbehera29@gmail.com>
2022-07-10 00:21:05 +0530

    🚨 eslint & prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<main className='font-poppins'>
-			<h1 className='text-pink-500 text-4xl text-center mt-16'>Henlo</h1>
-		</main>
-	);
+  return (
+    <main className="font-poppins">
+      <h1 className="text-pink-500 text-4xl text-center mt-16">Henlo</h1>
+    </main>
+  );
 }
 
 export default App;
