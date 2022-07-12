207620195565e583faa5ad61d19b914cdd0da00a
Shrey Maurya <shreymaurya2000@gmail.com>
2022-07-12 23:33:08 +0530

    🎇Navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import ChatRoom, { SignIn } from './components/ChatRoom';
 import { auth } from './Firebase';
+import Navbar from './Navbar';
 function App() {
   const [user] = useAuthState(auth);
   return (
-    <div className="App">
+    <div className="w-screen h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">
       <header>
         <h1>⚛️🔥💬</h1>
       </header>
       <section>{user ? <ChatRoom /> : <SignIn />}</section>
+
+      <Navbar />
     </div>
   );
 }
